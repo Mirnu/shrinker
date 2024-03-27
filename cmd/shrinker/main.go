ba54c84d017616d60e28add9324ec82390991469
Mernya <kirillprofi007@gmail.com>
2024-03-27 13:06:54 +0300

    initial

---
+++ cmd/shrinker/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"shrinker/internal/config"
+	"shrinker/internal/lib/logger/sl"
+	"shrinker/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("cannot create storage: %w", sl.Error(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
